ca87c7ebf9f28d68994ff0d3e04dfdabb2e50601
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2021-06-01 20:48:27 -0700

    something

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(methodOverride());
 
-let auth = require('./auth')(app);
+// let auth = require('./auth')(app);
 
 const passport = require('passport');
 require('./passport');
@@ -346,6 +346,24 @@ app.get('/documentation', (req, res) => {
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
+});
+
+app.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+        if (error || !user) {
+            return res.status(400).json({
+                message: 'Something is not right',
+                user: user
+            });
+        }
+        req.login(user, { session: false }, (error) => {
+            if (error) {
+                res.send(error);
+            }
+            let token = generateJWTToken(user.toJSON());
+            return res.json({ user, token });
+        });
+    })(req, res);
 });
 
 // listen for requests
